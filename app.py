32bfd1c258443eed19c254c7810db6e3bacb5ad3
ibnatiqbal <ibbieibnat@gmail.com>
2015-02-21 16:20:14 -0500

    converted tabs to spaces in app.py

--- app.py
+++ app.py
@@ -8,37 +8,37 @@ def hello():
 
 @app.route("/calendar")
 def calendar():
-	empty = {
-		day_of_week: "monurday",
-		day_of_month: "32",
-		month:"marchuary",
-		activities: []
-	}
+    empty = {
+        day_of_week: "monurday",
+        day_of_month: "32",
+        month:"marchuary",
+        activities: []
+    }
 
-	fancyday = {
-		day_of_week: "wednesday",
-		day_of_month: "15",
-		month: "February",
-		activities: [
-						{
-							time:localtime(),
-							category: "basketball",
-							title: "intramural bball",
-						},
-						{
-							time:localtime(),
-							category: "football",
-							title: "varsity football"
-						}
-					]
-	}
+    fancyday = {
+        day_of_week: "wednesday",
+        day_of_month: "15",
+        month: "February",
+        activities: [
+                        {
+                            time:localtime(),
+                            category: "basketball",
+                            title: "intramural bball",
+                        },
+                        {
+                            time:localtime(),
+                            category: "football",
+                            title: "varsity football"
+                        }
+                    ]
+    }
 
-	aim = [
-		[empty, empty, fancyday, empty, empty],
-		[empty, empty, empty, empty, empty],
-		[empty, empty, empty, empty, fancyday],
-		[empty, empty, empty, empty, empty],
-	]
+    aim = [
+        [empty, empty, fancyday, empty, empty],
+        [empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, fancyday],
+        [empty, empty, empty, empty, empty],
+    ]
 
     return render_template('calendar.html', activities_in_month=aim)
 
