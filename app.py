8147e3bff6179d2bfc647c7ccbcc381b90075f48
Ben Sussman <benjamin.sussman@gmail.com>
2015-02-21 13:52:36 -0500

    added template folder and empty calendar template with empty calendar controller action

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/calendar")
+def calendar():
+    return render_template('calendar.html')
+
 
 if __name__ == "__main__":
     app.run()
---
+++ templates/calendar.html
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        TESTING
+    </body>
+</html>
